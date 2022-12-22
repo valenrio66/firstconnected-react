255d64dbb8619ca14f1ce8281d9c11ab110e3735
Valen Rionald <vrionald@gmail.com>
2022-12-22 15:50:30 +0700

    Update Axios

--- src/index.js
+++ src/index.js
@@ -1,11 +1,40 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import reportWebVitals from "./reportWebVitals";
+import axios from 'axios';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const endpoints = [
+  "https://test-gogin.herokuapp.com/invertebratas",
+  "https://test-gogin.herokuapp.com/fosils",
+  "https://test-gogin.herokuapp.com/vertebratas",
+  "https://test-gogin.herokuapp.com/villages",
+];
+axios.all(endpoints.map((endpoint) => axios.get(endpoint))).then((allResponses) => {
+    allResponses.forEach((response) => {
+    console.log(response.data);
+  });
+});
+
+// axios({
+//   method: "get",
+//   url: "http://test-gogin.herokuap.com",
+// }).then(function (response) {
+//   console.log(response.data);
+// });
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// reportWebVitals();
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
