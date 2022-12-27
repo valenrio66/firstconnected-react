84394aba483444a71fa6aa34e830f223fa9d2cec
Valen Rionald <vrionald@gmail.com>
2022-12-27 16:16:10 +0700

    Try Invertebratas

--- src/components/Invertebratas.js
+++ src/components/Invertebratas.js
@@ -1,4 +1,12 @@
+import React, { Component, Fragment } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
+// const axios = require('axios');
+// axios
+//   .get("https://finalspaceapi.com/api/v0/character/?limit=2")
+//   .then(function (response) {
+//     console.log(response);
+//   });
 
 axios.get('https://test-gogin.herokuapp.com/invertebratas')
     .then(Response => {
